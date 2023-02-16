78e5de55b66925f094b332798c1a7bde0c9548d4
edwinatieno <edwinmuthee11@gmail.com>
2023-02-16 23:05:36 +0300

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,54 @@
+
+
+
+
+
 #include <stdio.h>
+
+
+
+
+
+
+
+
+
+/**
+
+
+* main functiO- Prints "Programming is like building a multilingual
+
+
+*                puzzle, followed by a new line.
+
+
+*
+
+
+* Return: Always 0.
+
+
+*/
+
+
 int main(void)
+
+
 {
-puts(“\”Programming is like building a multilingual puzzle”);
-return (0);
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+
+
+
+
+
+
+	return (0);
+
+
 }
+
+
 
