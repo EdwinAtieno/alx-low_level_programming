a3390ba0a0b2abbeb2e390e3b12e77777f0a5c5b
edwinatieno <edwinmuthee11@gmail.com>
2023-02-16 23:28:12 +0300

    file 5 & 6 edited and formatted by betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Uses printf function
+ *
+ * Return: gives 0 to show success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,25 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - start function for displaying integers
+ *
+ * Return: gives 0
+ */
+
+
+int main(void)
+{
+	char a;
+int num;
+	long int b;
+	long long int c;
+	float d;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
+
