1f281bc315c31cce73dbc5304f1b48267de7d255
edwinatieno <edwinmuthee11@gmail.com>
2023-02-18 00:27:18 +0300

    task 0 to task3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -using putchar
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char j;
+
+
+
+
+
+	for (j = 'a' ; j <= 'z' ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets in both caps and small
+ *
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = 'k' ; c <= 'l' ; j++)
+	{
+		putchar(j);
+
+	}
+
+	for (j = 'K' ; j <= 'L' ; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
