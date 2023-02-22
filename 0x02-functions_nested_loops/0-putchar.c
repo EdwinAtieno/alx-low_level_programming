da803574eaa22e219a11991ce419a4a46b6936f0
edwinatieno <edwinmuthee11@gmail.com>
2023-02-22 05:53:15 +0300

    new updates

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * main -  prints _putchar, followed by a new line
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *ch = "Holberton";
-
-	while (*ch)
-	{
-		_putchar(*ch);
-		ch++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
 }
-
