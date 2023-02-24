2975251a06b4b70f4d11e2394ffa100151b932f7
edwinatieno <edwinmuthee11@gmail.com>
2023-02-24 23:17:17 +0300

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,40 @@
-#include "main.h"
+
+#include "holberton.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints a number with _putchar
+ * @n: Number to print
+ *
+ * Return: On success 1 but
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	int ten, i, neg;
+	unsigned int digit, digit2;
 
+	ten = 1;
 	if (n < 0)
 	{
-		n1 = -n;
+		n = n * -1;
+		neg = 1;
+	}
+	digit = n;
+	digit2 = n;
+	while (digit >= 10)
+	{
+		digit = digit / 10;
+		ten = ten * 10;
+	}
+	if (neg == 1)
+	{
 		_putchar('-');
-	} else
-	{
-		n1 = n;
 	}
-
-	if (n1 / 10)
+	_putchar('0' + (digit2 / ten));
+	i = ten / 10;
+	while (i >= 1)
 	{
-		print_number(n1 / 10);
+		_putchar('0' + (digit2 / i) % 10);
+		i = i / 10;
 	}
-
-	_putchar((n1 % 10) + '0');
 }
-
