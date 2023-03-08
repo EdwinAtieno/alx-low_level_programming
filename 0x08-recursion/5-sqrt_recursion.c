495f5f6b80f7d735c4df486ba1cc24add313d2f9
edwinatieno <edwinmuthee11@gmail.com>
2023-03-08 13:44:34 -0800

    task 5 and 6 updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (actual_sqrt(n, 0));
 }
 
 /**
@@ -27,5 +27,5 @@ int actual_sqrt(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (actual_sqrt(n, i + 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n - 1));
+	return (is_prime(n, n - 1));
 }
 
 /**
@@ -27,5 +27,5 @@ int is_prime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actual_prime(n, i - 1));
+	return (is_prime(n, i - 1));
 }
