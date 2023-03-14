18a11678b0af33f514b938c51f7774ed5cda3a9c
edwinatieno <edwinmuthee11@gmail.com>
2023-03-14 11:45:49 -0700

    It's not bragging if you can back it up mandatory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated
+ *space in memory, which contains a copy of the
+ *string given as a parameter.
+ *@str:String to be copied
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *cpy;
+	int index, len;
+
+	if (str == NULL)
+		return (NULL);
+	/*determining length of string given*/
+	for (index = 0; str[index]; index++)
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+
+	if (cpy == NULL)
+		return (NULL);
+	/* copying @str string into the new string(cpy)*/
+	for (index = 0; str[index]; index++)
+	{
+		cpy[index] = str[index];
+	}
+
+	cpy[len] = '\0';
+
+	return (cpy);
+}
